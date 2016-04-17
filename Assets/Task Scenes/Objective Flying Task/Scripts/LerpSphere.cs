9bea9a8e7a7bfe68783be205df81ebd265350490
benjamon <benjampeterson@gmail.com>
2016-04-16 20:14:02 -0700

    Added prototype brightness, contrast, sort of hue

---
+++ Assets/Task Scenes/Objective Flying Task/Scripts/LerpSphere.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LerpSphere : MonoBehaviour {
+    public Transform leadTransform;
+
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.Lerp(transform.position, leadTransform.position, .15f);
+	}
+}
---
+++ Assets/Task Scenes/Shared Assets/Shared Scripts/CameraColorShift.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraColorShift : MonoBehaviour {
+    [Range(0,1)]
+    public float brightness = .5f, contrast = .5f, hue = .5f;
+    public ColorSuite colorSuite;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float difference = 1 - (contrast * 2);
+        colorSuite.rgbCurve = getCurve(0, 1, 0 + difference * brightness + (brightness - .5f), 1 - difference + (brightness - .5f));
+        colorSuite.redCurve = getCurve(0, 1, 0, 1 + (hue * 5 - 2.5f) / 10);
+	}
+
+    AnimationCurve getCurve(float startTime, float EndTime, float startValue, float EndValue)
+    {
+        Keyframe[] newKC = new Keyframe[2];
+        newKC[0] = new Keyframe(startTime, startValue);
+        newKC[1] = new Keyframe(EndTime, EndValue);
+        return new AnimationCurve(newKC);
+    }
+}
